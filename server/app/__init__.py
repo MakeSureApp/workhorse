48eeec8e006171553c6241b751afda3c13231322
Vadim Voloschuk <vvoloschuk@irbis-tech.ru>
2024-02-03 21:26:08 +0300

    simple defence

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,8 +3,14 @@ import secrets
 from common.pereferences import DBHOST, DBUSER, DBPASSWORD, DBNAME, DBPORT
 import mysql.connector
 
+from prometheus_flask_exporter import PrometheusMetrics
+
+
+
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
+
+metrics = PrometheusMetrics(app)
 
 db_config = {
     'host': DBHOST,
