b71824dbdf694e687b24714c9181b617dd3257b8
Vadim Voloschuk <vvoloschuk@irbis-tech.ru>
2024-02-03 21:27:35 +0300

    simple defence

--- server/run.py
+++ server/run.py
@@ -1,8 +1,4 @@
 from app import app
 from common.pereferences import DEBUG, PORT, HOST, THREADED
 
-from prometheus_flask_exporter import PrometheusMetrics
-
-metrics = PrometheusMetrics(app)
-
 app.run(HOST, PORT, debug=DEBUG, threaded=THREADED)
