385578ba0252e2b8e0f9abe4d61958fec3b960d8
Волощук Вадим Игоревич <kingofsweetsx2k@gmail.com>
2024-07-10 23:21:44 +0300

    demo video

--- server/app/views.py
+++ server/app/views.py
@@ -28,7 +28,7 @@ from common.helpers import convert_to_cv2, convert_to_pillow
 
 @app.before_request
 def check_api_key():
-    if request.endpoint not in ['static', 'checker', 'metrics', '/metrics', 'prometheus_metrics']:
+    if request.endpoint not in ["/index","index",'static', "test_results_detection", "/test_results_detection",'checker', 'metrics', '/metrics', 'prometheus_metrics']:
         print('Имя эндпоинта:', request.endpoint)
         api_key = request.headers.get('Api-Key')
 
